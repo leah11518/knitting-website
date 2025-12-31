c5147a2dd8e0ec55660fc94506765e461d2f60e6
Leah Ball <leah.g.ball6@gmail.com>
2025-12-31 12:04:43 -0500

    value type

--- knitting-website/src/App.jsx
+++ knitting-website/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const blob = "string";
+  const integer = 2;
+  const array = ["string", "kfdf"];
+  const dictionary = { key: integer, key2: blob, key3: array };
 
   return (
     <>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
